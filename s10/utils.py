4a17b6c3a370c865df419a36fdf1ea2df8907466
11kartheek <72185894+11kartheek@users.noreply.github.com>
2024-04-04 14:17:58 +0530

    Update utils.py

--- s10/utils.py
+++ s10/utils.py
@@ -29,7 +29,7 @@ class CutoutAfterToTensor(object):
         img = img * mask + (1 - mask) * self.fill_color[:, None, None]
         return img
 
-
+cutout_size = 8
 # Train Phase transformations
 train_transforms = transforms.Compose(
     [
@@ -39,7 +39,8 @@ train_transforms = transforms.Compose(
         transforms.Normalize(
             (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
         ),  # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
-        CutoutAfterToTensor(1, 8),
+        # CutoutAfterToTensor(1, 8),
+        transforms.RandomErasing(p=0.5, scale=(cutout_size / 32, cutout_size / 32), ratio=(1, 1))
     ]
 )
 
